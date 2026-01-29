041f1afde007076257dea8d3a35e2aec17849247
Acbox <acbox0328@gmail.com>
2026-01-29 14:49:25 +0800

    feat: add config load to agent gateway

---
+++ agent/src/config.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from 'fs'
+import { parse } from 'toml'
+
+type AgentGatewayConfig = {
+  'agent_gateway': {
+    host?: string
+    port?: number
+  }
+}
+
+export const loadConfig = (path: string = './config.toml'): AgentGatewayConfig => {
+  const config = parse(readFileSync(path, 'utf-8'))
+  return config satisfies AgentGatewayConfig
+}
--- agent/src/index.ts
+++ agent/src/index.ts
@@ -2,12 +2,18 @@ import { Elysia } from 'elysia'
 import { chatModule } from './modules/chat'
 import { corsMiddleware } from './middlewares/cors'
 import { errorMiddleware } from './middlewares/error'
+import { loadConfig } from './config'
+
+const config = loadConfig('../config.toml')
 
 const app = new Elysia()
   .use(corsMiddleware)
   .use(errorMiddleware)
   .use(chatModule)
-  .listen(8081)
+  .listen({
+    port: config.agent_gateway.port ?? 8081,
+    hostname: config.agent_gateway.host ?? '127.0.0.1',
+  })
 
 console.log(
   `Agent Gateway is running at ${app.server?.hostname}:${app.server?.port}`
